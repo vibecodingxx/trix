7e556e61420fadd1178116f1d0c409953bfd0065
Vibe Coding <fazligismail@gmail.com>
2026-06-13 18:13:54 +0800

    Add files via upload

--- others/certv2ray.sh
+++ others/certv2ray.sh
@@ -26,6 +26,7 @@ domain=$(cat /usr/local/etc/xray/domain)
 emailcf=$(cat /usr/local/etc/xray/email)
 clear
 systemctl stop xray
+pkill xray
 echo -e "\e[0;32mStart renew your Certificate SSL\e[0m"
 sleep 1
 /root/.acme.sh/acme.sh --upgrade --auto-upgrade
@@ -33,6 +34,7 @@ sleep 1
 /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
 ~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /usr/local/etc/xray/xray.crt --keypath /usr/local/etc/xray/xray.key --ecc
 systemctl start xray
+restart
 echo Done
 sleep 0.5
 #echo -e "\e[0;32m All finished ! \e[0m" 
